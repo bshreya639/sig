43ce7a00abfffc564eab2ee89948adacf5b1ddc5
bshreya639 <bshreya639@gmail.com>
2023-07-06 19:39:23 +0200

    Update script.sh

--- script_for_deployment_automation/script_deployment/script.sh
+++ script_for_deployment_automation/script_deployment/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir -p /var/www/html
-
+sudo rm -rf /var/www/html/index*
 sudo /bin/cp -rf response.php /var/www/html/response.php
 sudo /bin/cp -rf display.php /var/www/html/display.php
 sudo /bin/cp -rf index.php /var/www/html/index.php
