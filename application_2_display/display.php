b56523dfcc5ee087771b414084e04c91bf27d928
bshreya639 <bshreya639@gmail.com>
2023-07-06 19:50:20 +0200

    Update display.php

--- application_2_display/display.php
+++ application_2_display/display.php
@@ -1,8 +1,6 @@
 <?php
 echo "<body style='background-color:yellow'>";
-$get_port=getenv('PORT');
-$get_ip=getenv('IP');
-$url = "http://$get_ip:$get_port/response.php";
+$url = "http://localhost:80/response.php";
 $homepage = file_get_contents($url);
 $get_json = json_decode($homepage);
 
