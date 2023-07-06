daf2a550f695c11f6cd7f481e3d535097069f399
bshreya639 <bshreya639@gmail.com>
2023-07-06 19:40:16 +0200

    Delete script_for_deployment_automation/script_deployment directory

--- script_for_deployment_automation/script_deployment/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-type: application/json');
-$response = array();
-$response[0] = array(
-    'id' => '1',
-    'message' => 'Hello world'
-);
-echo json_encode($response);
--- script_for_deployment_automation/script_deployment/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /var/www/html
-sudo rm -rf /var/www/html/index*
-sudo /bin/cp -rf response.php /var/www/html/response.php
-sudo /bin/cp -rf display.php /var/www/html/display.php
-sudo /bin/cp -rf index.php /var/www/html/index.php
-
-sudo chmod -R 755 /var/www/html/
-sudo chown -R www-data:www-data /var/www/html
